bce0a3ec7e5024b544fd638684ba120a6a78931d
sakibhossain <sakibhossainceo@gmail.com>
2023-10-08 09:58:44 -0700

    user data added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExternamUsers></ExternamUsers>
     </div>
   );
+}
+
+function ExternamUsers() {
+  const [users, setUsers] = useState([])
+  useEffect( () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+
+  }, [])
+  return (
+    <div>
+      <h1>External Users</h1>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props){
+  return (
+    <div style={{border: '2px solid red', margin: '10px'}}>
+      <h3>Name: {props.name}</h3>
+      <p>Email: {props.email}</p>
+    </div>
+  )
 }
 
 function Counter(){
@@ -17,7 +46,7 @@ function Counter(){
   //   const newCount = count + 1;
   //   setCount(newCount);
   // }
-  // sort way
+  // sort way ---
   const increaseCount = () => setCount(count + 1);
   const decreaseCount = () => setCount(count - 1);
 
